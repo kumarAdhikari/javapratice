40aaedbaf385a4fb4fb66f436b68b6aaf571828b
kumar adhikari <kumaradhikari7@gmail.com>
2022-03-07 16:58:37 -0600

    finally made connection and got response

--- Weatherreporterkumar.java
+++ Weatherreporterkumar.java
@@ -1,10 +1,17 @@
 package javapratice;
 
 
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.http.HttpResponse;
 import java.util.Scanner;
 
 public class Weatherreporterkumar {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Welcome to Kumar Weather Reporting Console App");
         System.out.println("------------------------------------------------");
 
@@ -12,8 +19,26 @@ public class Weatherreporterkumar {
         System.out.println("Please Enter the City you want: ");
         String city = scanner.nextLine();
         System.out.println("Getting Weather report for " + city);
+        String keyAPI = "e408964eb2206f1573c77832841e6b11";
+
+        URL url = new URL("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=e408964eb2206f1573c77832841e6b11");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+
+        BufferedReader responses = new BufferedReader(
+                new InputStreamReader(connection.getInputStream()));
+        String data;
+        StringBuffer content = new StringBuffer();
+        while ((data = responses.readLine()) != null) {
+            content.append(data);
+        }
+        System.out.println(content);
+
+
+
+        //closing connection with api
+        responses.close();
+        connection.disconnect();
     }
 }
-
-
 
