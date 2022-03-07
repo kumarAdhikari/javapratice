ce6fd3fa0398e4f2e32ff1fe34f31c8cff0a91fb
kumar adhikari <kumaradhikari7@gmail.com>
2022-03-07 15:24:30 -0600

    first update

--- Weatherreporterkumar.java
+++ Weatherreporterkumar.java
@@ -1,7 +1,19 @@
 package javapratice;
 
+
+import java.util.Scanner;
+
 public class Weatherreporterkumar {
     public static void main(String[] args) {
+        System.out.println("Welcome to Kumar Weather Reporting Console App");
+        System.out.println("------------------------------------------------");
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please Enter the City you want: ");
+        String city = scanner.nextLine();
+        System.out.println("Getting Weather report for " + city);
     }
 }
+
+
+
